b0b608e54ea11332f7e6f036922eeed1d97b7fc6
Gevorg Abgaryan <gevorg.gak@gmail.com>
2024-06-06 00:46:48 +0400

    add mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(
+      `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`
+    )
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
